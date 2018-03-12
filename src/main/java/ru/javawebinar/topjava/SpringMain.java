dfff3e5539f779b083f223fb99eb5f0a0d689c74
Valeriu <valeraky@yandex.ru>
2018-03-13 02:27:11 +0300

    Apply patch 2.6

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
-import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Arrays;
 
